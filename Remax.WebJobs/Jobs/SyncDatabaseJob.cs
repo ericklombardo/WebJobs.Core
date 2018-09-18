853fa30a17b94b418c4a17a03eccb90f7faadee1
Erick Lombardo Núñez Aguilar <lombardo79@gmail.com>
2018-09-18 16:27:19 -0600

    add syncdatabase job initial class

--- Remax.WebJobs/Jobs/SyncDatabaseJob.cs
+++ Remax.WebJobs/Jobs/SyncDatabaseJob.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
 
 namespace Remax.WebJobs.Jobs
 {
     public class SyncDatabaseJob
     {
+        public async Task SyncDatabase([QueueTrigger("%SyncDatabaseQueue%")] string json)
+        {
+
+        }
     }
 }
